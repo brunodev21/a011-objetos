0818857837b7524a725a49939f5494729154b7c9
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-09-05 14:48:43 -0300

    primeiro commit

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const sacolao = []
+
+const fruta1 = {
+    nome: "Maça",
+    preco: 2,
+    disponibilidade: true,
+}
+
+const fruta2 = {
+    nome: "Melancia",
+    preco: 7,
+    disponibilidade: false,
+}
+
+const fruta3 = {
+    nome: "Tangerina",
+    preco: 3,
+    disponibilidade: true,
+}
+
+sacolao.push(fruta1, fruta2, fruta3)
+
+console.log(sacolao)
+    
